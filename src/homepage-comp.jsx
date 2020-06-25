f8139b2f487d0c6174764ff84657e854c2be8d3f
HaofangLiu <494256972@qq.com>
2020-06-25 15:40:17 +0800

    created project with create react app with added homepage file

---
+++ src/homepage-comp.jsx
@@ -0,0 +1,43 @@
+import React from "react";
+import "./homepage.scss";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <div className="directory-menu">
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">吃饱了撑的</h1>
+            <span className="subtitle">来看看？</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">闲出来的</h1>
+            <span className="subtitle">来看看？</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">菜的真实</h1>
+            <span className="subtitle">来看看？</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">菜的真实</h1>
+            <span className="subtitle">来看看？</span>
+          </div>
+        </div>
+        <div className="menu-item">
+          <div className="content">
+            <h1 className="title">菜的真实</h1>
+            <span className="subtitle">来看看？</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
